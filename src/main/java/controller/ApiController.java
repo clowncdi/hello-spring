87e9fb6552347dfd333d42ae38c8f2c5f72ecc8d
daeil.choi <daeil.choi@classact.co.kr>
2021-05-12 13:30:00 +0900

    Initial commit

---
+++ src/main/java/controller/ApiController.java
@@ -0,0 +1,10 @@
+package controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController  // 해당 Class 는 REST API 처리하는 Controller
+@RequestMapping("/api")  // ReqeustMapping URI 를 지정해주는 Annotation
+public class ApiController {
+
+}
