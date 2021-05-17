fa0ee4808212c4895fdd5e597c7781dc5e86f9f8
daeil.choi <daeil.choi@classact.co.kr>
2021-05-17 09:43:08 +0900

    Object Mapper

--- src/main/java/com/example/hello/dto/User1.java
+++ src/main/java/com/example/hello/dto/User1.java
@@ -1,22 +1,27 @@
 package com.example.hello.dto;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
-@Setter
 @ToString
-@JsonNaming(value = PropertyNamingStrategy.SnakeCaseStrategy.class)
-// @JsonInclude(JsonInclude.Include.NON_NULL)  // null 포함 안함
-public class User {
+@AllArgsConstructor
+@NoArgsConstructor
+public class User1 {
 	private String name;
 	private Integer age;
+
+	@JsonProperty("phone_number")
 	private String phoneNumber;
-	private String address;
+
+	public User1 defaultUser() {
+		return new User1("default", 0, "010-1111-2222");
+	}
 }
--- src/test/java/com/example/hello/HelloApplicationTests.java
+++ src/test/java/com/example/hello/HelloApplicationTests.java
@@ -3,11 +3,36 @@ package com.example.hello;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.example.hello.dto.User1;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @SpringBootTest
 class HelloApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws JsonProcessingException {
+		System.out.println("------------------");
+
+		// Object Mapper
+		// Text JSON -> Object
+		// Object -> Text JSON
+
+		// controller req json(text) -> object
+		// response object -> json(text)
+
+		var objectMapper = new ObjectMapper();
+
+		// object -> text
+		// object mapper get method 를 활용한다. object mapper를 이용할 땐 클래스 내부 메서드명에 get을 사용하면 안된다.
+		var user = new User1("steve", 10, "010-1111-2222");
+		var text = objectMapper.writeValueAsString(user);
+		System.out.println(text);
+
+		// text -> object
+		// object mapper 는 default 생성자를 필요로 한다.
+		var objectUser = objectMapper.readValue(text, User1.class);
+		System.out.println(objectUser);
 	}
 
 }
