383868b8670a8e49075b260be7a34aa5f91f1a18
daeil.choi <daeil.choi@classact.co.kr>
2021-05-17 12:28:01 +0900

    AOP 실무사례2

--- src/main/java/com/example/aop/controller/RestApiController.java
+++ src/main/java/com/example/aop/controller/RestApiController.java
@@ -1,13 +1,18 @@
 package com.example.aop.controller;
 
+import org.springframework.util.StopWatch;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.aop.annotation.Decode;
+import com.example.aop.annotation.Timer;
 import com.example.aop.dto.User;
 
 @RestController
@@ -25,6 +30,27 @@ public class RestApiController {
 	@PostMapping("/post")
 	public User post(@RequestBody User user) {
 		// System.out.println("post method : " + user);
+		return user;
+	}
+
+	@Timer
+	@DeleteMapping("/delete")
+	public void delete() throws InterruptedException {
+		// StopWatch stopWatch = new StopWatch();	// 횔단 관심사 AOP
+		// stopWatch.start();
+
+		// db logic
+		Thread.sleep(1000*2);
+
+		// stopWatch.stop();
+		// System.out.println("total time : " + stopWatch.getTotalTimeSeconds());
+	}
+
+	@Decode
+	@PutMapping("/put")
+	public User put(@RequestBody User user) {
+		System.out.println("put");
+		System.out.println(user);
 		return user;
 	}
 }
