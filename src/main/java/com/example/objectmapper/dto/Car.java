eb9e9caa0d46c0affa1705b809ef556123ac7f8f
daeil.choi <daeil.choi@classact.co.kr>
2021-05-18 16:09:19 +0900

    ObjectMapper 실무 사례

---
+++ src/main/java/com/example/objectmapper/dto/Car.java
@@ -0,0 +1,18 @@
+package com.example.objectmapper.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Car {
+	private String name;
+	@JsonProperty("car_number")
+	private String carNumber;
+	@JsonProperty("TYPE")
+	private String type;
+}
