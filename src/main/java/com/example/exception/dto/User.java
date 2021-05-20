8c1ebde5ff283cb9553282980c38e6920104b46b
daeil.choi <daeil.choi@classact.co.kr>
2021-05-20 10:29:07 +0900

    Spring Boot Exception 처리

---
+++ src/main/java/com/example/exception/dto/User.java
@@ -0,0 +1,23 @@
+package com.example.exception.dto;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class User {
+	@NotEmpty
+	@Size(min = 1, max = 10)
+	private String name;
+
+	@Min(1)
+	@NotNull
+	private Integer age;
+}
