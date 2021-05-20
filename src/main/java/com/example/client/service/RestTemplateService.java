6fe407ddbbe36f3d86cca7f867ccb06b8cae6f6b
daeil.choi <daeil.choi@classact.co.kr>
2021-05-20 16:05:44 +0900

    Rest Template 사용하기 Post

--- src/main/java/com/example/client/service/RestTemplateService.java
+++ src/main/java/com/example/client/service/RestTemplateService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import com.example.client.dto.UserRequest;
 import com.example.client.dto.UserResponse;
 
 @Service
@@ -34,5 +35,33 @@ public class RestTemplateService {
 
 		return result.getBody();
 
+	}
+
+	public UserResponse post() {
+		// http://localhost:8080/api/server/user/{userId}/name/{userName}
+
+		URI uri = UriComponentsBuilder
+				.fromUriString("http://localhost:8080")
+				.path("/api/server/user/{userId}/name/{userName}")
+				.encode()
+				.buildAndExpand(100, "steve")
+				// .build()
+				// .expand(100, "steve")
+				.toUri();
+		System.out.println(uri);
+
+		// http body -> object -> object mapper -> json -> rest template -> http body json
+		UserRequest req = new UserRequest();
+		req.setName("steve");
+		req.setAge(10);
+
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<UserResponse> response = restTemplate.postForEntity(uri, req, UserResponse.class);
+
+		System.out.println(response.getStatusCode());
+		System.out.println(response.getHeaders());
+		System.out.println(response.getBody());
+
+		return response.getBody();
 	}
 }
