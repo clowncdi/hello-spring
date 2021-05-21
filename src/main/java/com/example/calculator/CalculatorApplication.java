fa207b777e3c846b9c73758a6b375584ebc9f748
daeil.choi <daeil.choi@classact.co.kr>
2021-05-21 17:17:12 +0900

    JUnit

---
+++ src/main/java/com/example/calculator/CalculatorApplication.java
@@ -0,0 +1,15 @@
+package com.example.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class CalculatorApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello JUnit");
+
+
+
+	}
+
+}
